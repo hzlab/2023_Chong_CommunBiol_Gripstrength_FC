022e4fac90df00e87c47e7c52f251ce8dbe8b9a8
joannachong <85987810+joannachong@users.noreply.github.com>
2024-01-11 22:53:30 +0800

    Add files via upload

--- Segregation/CalculateSegregation.m
+++ Segregation/CalculateSegregation.m
@@ -73,7 +73,7 @@ end
 
 %% Write segregation measures of all subjects to a csv file
 SegregationT=array2table([sublist, num2cell([globalseg, seg])],'VariableNames',['SubID','Global', networknames]);
-writetable(SegregationT, [outputdir, 'Segregationmeasures.csv']);    
+writetable(SegregationT, outputfile);    
 
 
 
